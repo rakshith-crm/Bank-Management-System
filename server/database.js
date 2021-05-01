a1a391fc6c904763cb4d570c64d3556fe790f678
rakshith-crm <77670199+rakshith-crm@users.noreply.github.com>
2021-05-01 18:34:15 +0530

    Update database.js

--- server/database.js
+++ server/database.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
     user: 'postgres',
-    password: 'kirukkupa',
+    password: 'arra1902',
     host: 'localhost',
     port: 5432,
     database: 'BANK'
